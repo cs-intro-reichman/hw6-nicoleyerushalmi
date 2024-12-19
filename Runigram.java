6da569fc3320943c7f4e4f294bf502746dc5356f
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2024-12-19 14:16:48 +0200

    5 try

--- Runigram.java
+++ Runigram.java
@@ -86,7 +86,7 @@ public class Runigram {
 		Color[][] new_image = new Color[image.length][image[0].length];
 		for(int i = 0; i< image.length; i++){
 			for(int j = 0; j<image[i].length; j++){
-				new_image[i][image.length - 1 - j] = image[i][j];
+				new_image[i][image[i].length - 1 - j] = image[i][j];
 			}
 		}
 		return new_image;
